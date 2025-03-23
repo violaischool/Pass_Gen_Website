7856cc96adc265cc739c72f7bebe91b20d29432e
viola <violaischool@gmail.com>
2025-03-23 21:34:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const lengthElement=document.getElementById("length");
+const upperCaseElement=document.getElementById("upperCase");
+const lowerCaseElement=document.getElementById("lowerCase");
+const numbersElement=document.getElementById("numbers");
+const symbolsElement=document.getElementById("symbols");
+const resultElement=document.getElementById("result");
+const generateElement=document.getElementById("generate");
+
+const UPPER_CASE=getArray("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+const LOWER_CASE=getArray("abcdefghijklmnopqrstuvwxyz");
+const NUMBERS =getArray("1234567890");
+const SYMBOLS =getArray("!@#$%^&*()_+~?|/*~>>/{};,:");
+
+function getArray(str){
+  return str.split("");
+}
+generate.add
+
